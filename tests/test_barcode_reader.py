380f687b8f40c9fc9d31349791042e591335fb5a
Nussi1 <59906591+Nussi1@users.noreply.github.com>
2024-01-21 23:43:16 +0600

    Update test_barcode_reader.py

--- tests/test_barcode_reader.py
+++ tests/test_barcode_reader.py
@@ -1,31 +1,38 @@
 import os
+
 import pytest
-from src.barcode_reader import BarcodeReader
+
 from config import properties
+from src.barcode_reader import BarcodeReader
+
 
 def test_compare_extracted_text_with_file_content():
     # Expected result
     expected_extracted_text = {
         "PN": "tst",
-        "DESCRIPTION": "PART LOCATION: 111 RECEIVER#: 9",
+        "SN": "123123",
+        "DESCRIPTION": "PART",
+        "LOCATION": "111",
+        "CONDITION": "FN",
+        "RECEIVER#": "9",
+        "UOM": "EA",
         "EXP DATE": "13.04.2022",
+        "PO": "P101",
         "CERT SOURCE": "wef",
         "REC.DATE": "18.04.2022",
+        "MFG": "efwfe",
         "BATCH#": "1",
+        "DOM": "13.04.2022",
         "REMARK": "",
+        "LOT#": "1",
         "TAGGED BY": "",
         "Qty": "1",
-        "SN": "123123",
-        "CONDITION": "FN UOM: EA",
-        "PO": "P101",
-        "MFG": "efwfe",
-        "DOM": "13.04.2022",
-        "LOT#": "1",
-        "NOTES": "inspection notes"
+        "NOTES": ""
     }
 
     # Create an instance of BarcodeReader
-    barcode_reader = BarcodeReader(properties.TEST_TASK)
+    barcode_reader = BarcodeReader(properties.TEST_TASK, properties.EXTRACTED_TEXT_FILE)
+    barcode_reader.extract_text_from_pdf()
 
     # Check the existence of the file
     assert os.path.exists(properties.EXTRACTED_TEXT_FILE)
@@ -34,6 +41,11 @@ def test_compare_extracted_text_with_file_content():
     with open(properties.EXTRACTED_TEXT_FILE, "r", encoding="utf-8") as file:
         extracted_text_content = file.read()
 
+    # Debugging prints
+    print("Extracted text content:")
+    print(extracted_text_content)
+    print("\nExpected text content:")
+    print("\n".join([f"{key}: {value}" for key, value in expected_extracted_text.items()]))
+
     # Compare the results
     assert barcode_reader.extracted_text == expected_extracted_text
-    assert extracted_text_content.strip() == str(expected_extracted_text).strip()
